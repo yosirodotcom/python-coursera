665f040e5c5a51b95f0abac4ea9fecc64faa11ac
yosiro <yosironadi@gmail.com>
2023-11-02 17:00:52 +0700

    update

--- playground.py
+++ playground.py
@@ -1,35 +1,6 @@
-# Soal 1
+import mouse
+import time
 
-
-def create_python_script(filename):
-    comments = "# Start of a new Python program"
-    with open(filename, "w") as file:
-        file.write(comments)
-
-    with open(filename, "r") as f:
-        filesize = len(f.readline())
-    return filesize
-
-
-print(create_python_script("program.py"))
-
-
-# Soal 4
-import os
-import datetime
-
-
-def file_date(filename):
-    # Create the file in the current directory
-    with open(filename, "w") as file:
-        pass
-    timestamp = os.path.getmtime(filename)
-    # Convert the timestamp into a readable format, then into a string
-    formattime = str(datetime.datetime.fromtimestamp(timestamp))
-    # Return just the date portion
-    # Hint: how many characters are in “yyyy-mm-dd”?
-    return "{:.10s}".format(formattime)
-
-
-print(file_date("newfile.txt"))
-# Should be today's date in the format of yyyy-mm-dd
+while True:
+    mouse.click()
+    time.sleep(10)  # Pause for 0.5 seconds before next click
